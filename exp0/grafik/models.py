e87fd91037530e6f4cc6a019061e4d7363d35ebf
Sylvain Bellemare <sylvain@github>
2012-07-31 17:36:23 +0200

    created models 'Graph' and 'Point'

--- exp0/grafik/models.py
+++ exp0/grafik/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Graph(models.Model):
+    name = models.CharField(max_length=200)
+    time = models.DateTimeField('time')
+
+class Point(models.Model):
+    graph = models.ForeignKey(Graph)
+    x = models.DecimalField(max_digits=9, decimal_places=4)
+    y = models.DecimalField(max_digits=9, decimal_places=4)
