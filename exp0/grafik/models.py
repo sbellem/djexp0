90566b93522c3f7a272f79bbdb4ebd36caebc56d
Sylvain Bellemare <sylvain@github>
2012-08-01 16:50:34 +0200

    adding __unicode__() methods to models

--- exp0/grafik/models.py
+++ exp0/grafik/models.py
@@ -4,7 +4,14 @@ class Graph(models.Model):
     name = models.CharField(max_length=200)
     time = models.DateTimeField('time')
 
+    def __unicode__(self):
+        return self.name
+
+
 class Point(models.Model):
     graph = models.ForeignKey(Graph)
     x = models.DecimalField(max_digits=9, decimal_places=4)
     y = models.DecimalField(max_digits=9, decimal_places=4)
+
+    def __unicode__(self):
+        return '(' + str(self.x) + ', ' + str(self.y) + ')'
