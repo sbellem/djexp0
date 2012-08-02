7096b834aba3a8f898237e37b2c35dc8ac723423
Sylvain Bellemare <sylvain@github>
2012-08-02 14:37:49 +0200

    adding Point model to admin site

--- exp0/grafik/admin.py
+++ exp0/grafik/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from grafik.models import Graph
+from grafik.models import Graph, Point
 
 
-admin.site.register(Graph)
+class PointInline(admin.TabularInline):
+    model = Point
+    extra = 1
+
+
+class GraphAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name']}),
+        ('Publication date', {'fields': ['time'], 'classes': ['collapse']}),
+    ]
+    inlines = [PointInline]
+
+
+admin.site.register(Graph, GraphAdmin)
